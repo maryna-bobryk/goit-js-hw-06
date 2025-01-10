7590b0010472b18961c2230340191ca0d80bee9a
Maryna Bobryk <marynabobryk@MacBook-Pro-von-Maryna.local>
2025-01-10 15:53:31 +0100

    fix task-2

--- js/task-2.js
+++ js/task-2.js
@@ -8,11 +8,11 @@ class Storage {
   }
 
   addItem(newItem) {
-    return this.items.push(newItem)
+    this.items.push(newItem)
   }
 
   removeItem(itemToRemove) {
-    return this.items = this.items.filter(item => item !== itemToRemove);
+    this.items = this.items.filter(item => item !== itemToRemove);
   }
 }
 
